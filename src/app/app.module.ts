827f0daba928c9d73e7c2aafbf8b2501b721d266
stevenjeri <stevenjeri33@gmail.com>
2021-07-15 21:53:58 +0300

    added new files, modified src

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
