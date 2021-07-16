3a821cf403a6391b8740421449b1d1e538188b50
stevenjeri <stevenjeri33@gmail.com>
2021-07-16 11:35:58 +0300

    modified tsconfig.json and modified src

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes = [
+    new Quote("MLK", "Martin Luther King", "Injustice anywhere is a threat to justice everywhere."),
+    new Quote("Life", "Margeret Atwood", "In the end we all become stories ")
+  ]
 
   constructor() { }
 
